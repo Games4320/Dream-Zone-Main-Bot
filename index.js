904a592d5f48b07ab7ae04168773f455c65f91eb
Games4320 <games4320@example.com>
2026-09-18 14:40:49 +0300

    Add BetterZonestaffapplication emoji back to message and button

--- index.js
+++ index.js
@@ -483,13 +483,14 @@ client.once(Events.ClientReady, async () => {
         .addFields(
           { name: '****תנאי קבלה:****', value: '`1. בגרות ואחראיות מלאה`\n\n`2. גיל 13+`\n\n`3. להיות אחד שבאמת רוצה לקדם את השרת.`', inline: false },
           { name: '\u200B', value: 'אזזז למה אתם מחכים? תתחילו בחינה!', inline: false },
-          { name: '\u200B', value: '-# כדי להתחיל בחינה יש ללחוץ על הכפתור למטה!', inline: false }
+          { name: '\u200B', value: '-# כדי להתחיל בחינה יש ללחוץ על הכפתור <:BetterZonestaffapplication:1522683237825249474> למטה!', inline: false }
         );
 
       const examButton = new ButtonBuilder()
         .setCustomId('exam_start')
         .setLabel('בחינה')
-        .setStyle('Secondary');
+        .setStyle('Secondary')
+        .setEmoji('1522683237825249474');
 
       const row = new ActionRowBuilder().addComponents(examButton);
 
